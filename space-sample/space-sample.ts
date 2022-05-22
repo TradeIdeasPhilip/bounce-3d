7d3e8e47af2a7c2559d6442e5e4da93e15d277dd
Philip Smolen <philip@trade-ideas.com>
2022-05-21 20:08:41 -0700

    Make the space-sample more clear.
    The lines in the image are longer.
    The diagonal parts are similar.
    But extend the lines up and down
    to make it more clear that the lines are sperating the characters.

--- space-sample/space-sample.ts
+++ space-sample/space-sample.ts
@@ -18,28 +18,29 @@ function divTest() {
   });
 }
 
-function drawLine(x1:number, y1: number, x2:number, y2:number) {
-  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  svg.appendChild(path);
-  path.setAttribute("d", `M ${x1}, ${y1} L ${x2}, ${y2}`);
-  path.setAttribute("stroke", "#808080");
-  path.setAttribute("stroke-width", "2px");
-  path.setAttribute("stroke-linecap", "round");
-}
-
 document.querySelectorAll(".example td").forEach(td => {
-  const topPadding = 6;
-  const bottomPadding = 6;
+  const padding = 6;
+  function drawPath(x1: number, y1: number, x2: number, y2: number) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    svg.appendChild(path);
+    path.setAttribute("d", `M ${x1},${y1 - padding * 4} L ${x1}, ${y1 - padding} ${x2},${y2 + padding} ${x2},${y2 + padding * 4}`);
+    path.setAttribute("stroke", "#aaaaaa");
+    path.setAttribute("stroke-width", "1.5px");
+    path.setAttribute("stroke-linecap", "round");
+    path.setAttribute("stroke-linejoin", "round");
+    path.setAttribute("fill", "none");
+  }
+
   const rows = td.querySelectorAll("div");
   if (rows.length != 2) {
     throw new Error("wtf");
   }
   for (const [n, top, bottom] of zip(count(), rows[0].querySelectorAll("span"), rows[1].querySelectorAll("span"))) {
     const topRect = top.getClientRects()[0];
-    const bottomRect = bottom.getClientRects()[0];    
-    drawLine(topRect.right, topRect.bottom - bottomPadding, bottomRect.right, bottomRect.top + topPadding);
+    const bottomRect = bottom.getClientRects()[0];
+    drawPath(topRect.right, topRect.bottom, bottomRect.right, bottomRect.top);
     if (n == 0) {
-      drawLine(topRect.left, topRect.bottom - bottomPadding, bottomRect.left, bottomRect.top + topPadding);
+      drawPath(topRect.left, topRect.bottom, bottomRect.left, bottomRect.top);
     }
   }
 })
